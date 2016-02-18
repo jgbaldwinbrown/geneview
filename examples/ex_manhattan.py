42cd0b0c9910a289056e5de68ec144c7704b0f38
Shujia Huang <huangshujia9@gmail.com>
2016-02-19 00:38:28 +0800

    update the examples

--- examples/ex_manhattan.py
+++ examples/ex_manhattan.py
@@ -4,16 +4,12 @@ import csv
 import matplotlib.pyplot as plt
 
 sys.path.append('..')  
-from geneview.gwas import manhattanplot
+import geneview as gv
 
-xtick = ['1', '2','3','4','5','6','7','8','9','10','11','12','13',
-         '14','16','18', '20','22']
-with open("data/test_data.csv") as f:
-    f_csv = csv.reader(f)
-    headers = next(f_csv)
-    data = [[row[0], int(row[2]), float(row[3])] for row in f_csv]
-
-ax = manhattanplot(data, xlabel="Chromosome", ylabel="-Log10(P-value)",
-                   xtick_label_set = set(xtick))
-
+xtick = map(str, range(1, 15) + ['16','18', '20','22'])
+df = gv.util.load_dataset('GOYA_preview')
+gv.gwas.manhattanplot(df[['chrID','position','pvalue']], 
+                      xlabel="Chromosome", 
+                      ylabel="-Log10(P-value)", 
+                      xtick_label_set = set(xtick))
 plt.show()
