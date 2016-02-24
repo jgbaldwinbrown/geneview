94bbe4c51f0791a8098993653543e533607b8ee7
Shujia Huang <huangshujia9@gmail.com>
2016-02-24 17:49:44 +0800

    modify example

--- examples/ex_karyoplot.py
+++ examples/ex_karyoplot.py
@@ -5,7 +5,6 @@ sys.path.append('..')
 import geneview as gv
 
 fig, ax = plt.subplots(figsize=(20, 5))
-gv.set_style(style='white')
 gv.karyoplot('/Users/LiuSiyang/iCodeSpace/Project/geneview-data/'
              'karyotype/karyotype_human_hg19.txt', ax=ax)
 plt.show()
