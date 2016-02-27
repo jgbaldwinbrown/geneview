cb7a34790edb4746c375216b92e60e08183a0a27
Shujia Huang <huangshujia9@gmail.com>
2016-02-27 15:38:53 +0800

    add a parameters for manhattanplot

--- examples/ex_manhattan.py
+++ examples/ex_manhattan.py
@@ -6,11 +6,12 @@ import matplotlib.pyplot as plt
 sys.path.append('..')  
 import geneview as gv
 
-xtick = map(str, range(1, 15) + ['16','18', '20','22'])
+xtick = ['chr'+c for c in map(str, range(1, 15) + ['16', '18', '20', '22'])]
 df = gv.util.load_dataset('GOYA_preview')
 gv.gwas.manhattanplot(df[['chrID','position','pvalue']], 
                       xlabel="Chromosome", 
-                      ylabel="-Log10(P-value)", 
+                      ylabel="-Log10(P-value)",
+                      xticklabel_kws={'rotation': 'vertical'},
                       xtick_label_set = set(xtick))
 plt.savefig('manhattan.png')
 plt.show()
