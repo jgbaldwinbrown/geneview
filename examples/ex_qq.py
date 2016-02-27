3f6adf17cabe78efd310bbbccd43ae0117294960
Shujia Huang <huangshujia9@gmail.com>
2016-02-27 12:13:57 +0800

    update the example

--- examples/ex_qq.py
+++ examples/ex_qq.py
@@ -5,8 +5,12 @@ sys.path.append('..')
 import geneview as gv
 
 df = gv.util.load_dataset('GOYA_preview')
-gv.gwas.qqplot(df['pvalue'], 
+gv.gwas.qqplot(df['pvalue']) 
+"""
+fig, ax = plt.subplots()
+gv.gwas.qqplot(df['pvalue'], ax=ax,
                xlabel="Expected p-value(-log10)", 
-               ylabel="Observed p-value(-log10)")    
+               ylabel="Observed p-value(-log10)")
+"""
 plt.savefig('qq.png')
 plt.show()
