524f66dfcc9ba7304ddca1ef03448aeef6b0be89
Shujia Huang <huangshujia9@gmail.com>
2016-03-12 21:54:56 +0800

    update

--- examples/ex_manhattan.py
+++ examples/ex_manhattan.py
@@ -9,6 +9,7 @@ import geneview as gv
 xtick = ['chr'+c for c in map(str, range(1, 15) + ['16', '18', '20', '22'])]
 df = gv.util.load_dataset('GOYA_preview')
 gv.gwas.manhattanplot(df[['chrID','position','pvalue']], 
+                      hline_kws={'y': 3, 'color': 'b', 'lw': 1},
                       xlabel="Chromosome", 
                       ylabel="-Log10(P-value)",
                       xticklabel_kws={'rotation': 'vertical'},
